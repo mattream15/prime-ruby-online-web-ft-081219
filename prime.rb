7fc398233fd6693c9d37d9c855706a0e0945209a
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-24 02:13:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,3 +6,15 @@ def prime?(integer)
     return false
 end
 end
+
+ function is_prime(n)
+     if n ≤ 3
+        return n > 1
+     else if n mod 2 = 0 or n mod 3 = 0
+        return false
+     let i ← 5
+     while i * i ≤ n
+        if n mod i = 0 or n mod (i + 2) = 0
+            return false
+        i ← i + 6
+     return true
