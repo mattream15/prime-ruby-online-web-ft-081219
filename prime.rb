104528ab7b8aa7addaede730a48b1b9fb5ac632e
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-24 02:54:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+ def prime?(n)
+   results = true
+   if n <= 1
+     return false
+   end
+   array = (2..(n-1)).to_a
+  results = array.none? do |num|
+     n % num == 0
+   end
+   return results
+ end
