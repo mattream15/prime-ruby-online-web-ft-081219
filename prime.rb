c53a51e60d6b58b9813999d87bfd2660d3e88b90
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-24 02:39:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,8 @@
  def prime?(n)
+   results = true
+   if n <= 1
+     return false
+   end
+   array(2..(n-1))
    
-     if n ≤ 3
-        return n > 1
-     else if n mod 2 = 0 or n mod 3 = 0
-        return false
-     let i ← 5
-     while i * i ≤ n
-        if n mod i = 0 or n mod (i + 2) = 0
-            return false
-        i ← i + 6
-     return true
+ end
