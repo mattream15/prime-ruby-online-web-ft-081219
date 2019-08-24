e31c832fb739ee4ce43ba8f926833883fe70208f
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-24 02:18:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,17 +6,3 @@ def prime?(integer)
     return false
 end
 end
-
- def prime?(integer)
-     if integer <= 3
-        return integer > 1
-     elsif integer mod 2 = 0 or integer mod 3 = 0
-        return false
-     let integer = 5
-     while integer * integer ≤ integer
-        if integer mod i = 0 or n mod (i + 2) = 0
-            return false
-        i ← i + 6
-     return true
-   end
- end
